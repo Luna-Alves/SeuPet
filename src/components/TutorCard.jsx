5e3bc77bf771b7f428146f1d459cab3d2d75d5dc
Luna ALves <lunaalves.pb@gmail.com>
2025-03-18 14:57:31 -0300

    ajustes em TutorCard

--- src/components/TutorCard.jsx
+++ src/components/TutorCard.jsx
@@ -3,19 +3,19 @@ import React from "react";
 function TutorCard({ tutor, onEdit, onDelete }) {
   return (
     <div className="tutor-card">
-      <h3>{tutor.name}</h3>
+      <h3>{tutor.nome}</h3>
       <p>
-        <strong>CPF:</strong> {tutor.cpf}
+        <strong>CPF:</strong> {tutor.documento}
       </p>
       <p>
         <strong>Email:</strong> {tutor.email}
       </p>
-      {/* <strong>Endereço:</strong> {tutor.rua}, {tutor.number}
+      {/* <strong>Endereço:</strong> {tutor.rua}, {tutor.numero}
       {tutor.complemento && `, ${tutor.complemento}`}, {tutor.bairro},{" "}
       {tutor.cidade} - {tutor.estado}, CEP: {tutor.cep}
       <button onClick={() => onEdit(tutor)}>Editar</button>
       <button onClick={() => onDelete(tutor.id)}>Excluir</button> */}
-      <strong>Endereço:</strong> {tutor.endereco.rua}, {tutor.endereco.number}
+      <strong>Endereço:</strong> {tutor.endereco.rua}, {tutor.endereco.numero}
       {tutor.endereco.complemento && `, ${tutor.endereco.complemento}`},{" "}
       {tutor.endereco.bairro}, {tutor.endereco.cidade} - {tutor.endereco.estado}
       , CEP: {tutor.endereco.cep}
